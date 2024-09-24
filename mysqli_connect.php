8c6caca51f7d44c11a8d55a5b907186e841178e6
Sophie Bell-Gray <92156579+sophiebellgray@users.noreply.github.com>
2024-09-24 12:29:52 +0100

    Update mysqli_connect.php
    removed database connection details

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,10 +1,10 @@
 <?php 
-// mudfoot (MySQL) login details
-DEFINE ('DB_USER', 'bellsoph'); 
-DEFINE ('DB_PASSWORD', 'Exlertha8'); 
-DEFINE ('DB_HOST', 'mudfoot.doc.stu.mmu.ac.uk');
-DEFINE ('DB_PORT', 6306);
-DEFINE ('DB_NAME', 'bellsoph'); 
+// mudfoot (MySQL) login details removed for privacy
+DEFINE ('DB_USER', ''); 
+DEFINE ('DB_PASSWORD', ''); 
+DEFINE ('DB_HOST', ');
+DEFINE ('DB_PORT', );
+DEFINE ('DB_NAME', ''); 
 
 $conn = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
 mysqli_set_charset($conn,"utf8");
